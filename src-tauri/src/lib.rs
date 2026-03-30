c2f7bd327339708c0c43ba0f586bc5033e7ccec6
Caden Zhang <w1nter.ish3re@gmail.com>
2026-03-30 11:00:11 -0700

    Tauri MCP

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -8,11 +8,18 @@ fn create_dir_all(path: &str) -> Result<(), String> {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
+    let mut builder = tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![create_dir_all])
+        .invoke_handler(tauri::generate_handler![create_dir_all]);
+
+    #[cfg(debug_assertions)]
+    {
+        builder = builder.plugin(tauri_plugin_mcp_bridge::init());
+    }
+
+    builder
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
