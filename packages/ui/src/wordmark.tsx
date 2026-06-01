7e0d1c5e112fe5c9fb8f3fc45e7d9e718f1cb31c
Caden Zhang <w1nter.ish3re@gmail.com>
2026-05-31 17:37:24 -0700

    Website and monorepo

---
+++ packages/ui/src/wordmark.tsx
@@ -0,0 +1,20 @@
+import { cn } from './utils';
+
+interface WordmarkProps {
+  className?: string;
+}
+
+function Wordmark({ className }: WordmarkProps) {
+  return (
+    <span
+      className={cn(
+        'font-heading font-semibold text-text-brand text-xl tracking-tight',
+        className,
+      )}
+    >
+      Myelin
+    </span>
+  );
+}
+
+export { Wordmark };
