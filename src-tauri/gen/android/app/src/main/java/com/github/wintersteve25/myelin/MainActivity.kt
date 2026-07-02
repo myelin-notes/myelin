f0068671e1fcc915ea5dcb3e1178363aff1eb1c0
winterSteve25 <w1nter.ish3re@gmail.com>
2026-07-01 22:04:26 -0700

    Android init

---
+++ src-tauri/gen/android/app/src/main/java/com/github/wintersteve25/myelin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.github.wintersteve25.myelin
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
