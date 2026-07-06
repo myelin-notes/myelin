5139e6a6242c6b93fb4a564b499cda9892d5e626
winterSteve25 <w1nter.ish3re@gmail.com>
2026-07-05 18:18:33 -0700

    Remove styling

--- packages/website/astro.config.mjs
+++ packages/website/astro.config.mjs
@@ -1,13 +1,8 @@
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
-import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://trymyelin.app',
-  integrations: [react(), sitemap()],
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  integrations: [sitemap()],
 });
