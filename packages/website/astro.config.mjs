5f0d3e0ccc897f072c8fb78329634fc2772aee3e
winterSteve25 <w1nter.ish3re@gmail.com>
2026-07-12 21:44:52 -0700

    delete more shit

--- packages/website/astro.config.mjs
+++ packages/website/astro.config.mjs
@@ -1,5 +1,5 @@
-import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
